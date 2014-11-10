5c4388853c65b4ace370017fbd693151cbbab7e9
SirDiazo <diazo@hotmail.com>
2014-11-09 19:45:14 -0700

    0.1
    Test release of ModuleGimbalActions

---
+++ ActsEW/ActsEW/Stock.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using KSP.IO;
+
+namespace ActsEW
+{
+    public class ModuleGimbalActions :PartModule
+    {
+        
+        
+        [KSPAction("Enable Gimbal")]
+        public void EnableGimbalAction(KSPActionParam param)
+        {
+            foreach (ModuleGimbal pm in this.part.Modules.OfType<ModuleGimbal>())
+            {
+                pm.FreeGimbal();
+            }
+        }
+
+        [KSPAction("Disable Gimbal")]
+        public void DisableGimbalAction(KSPActionParam param)
+        {
+            foreach (ModuleGimbal pm in this.part.Modules.OfType<ModuleGimbal>())
+            {
+                pm.LockGimbal();
+            }
+        }
+    }
+}
